02be912fbb04305a0321f263b77b315282096714
LeslieLargo1 <133000133+LeslieLargo1@users.noreply.github.com>
2023-06-20 15:25:26 +0200

    jjj

---
+++ script.js
@@ -0,0 +1,12 @@
+const container = document.querySelector('.DessertSection');
+  const overlay = container.querySelector('.dessertgallery');
+  
+
+  container.addEventListener('mouseover', () => {
+    overlay.style.display = 'block';
+  });
+
+  container.addEventListener('mouseout', () => {
+    overlay.style.display = 'none';
+  });
+  
